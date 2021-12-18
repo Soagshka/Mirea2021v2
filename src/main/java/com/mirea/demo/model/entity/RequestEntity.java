24358ae5f8170fb273add25a727cb76bf78417a8
soagshka <alexandergorbenko1996@gmail.com>
2021-12-18 12:41:26 +0300

    Lesson-3

---
+++ src/main/java/com/mirea/demo/model/entity/RequestEntity.java
@@ -0,0 +1,23 @@
+package com.mirea.demo.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "request")
+@Data
+public class RequestEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "request_id_generator")
+    @SequenceGenerator(name = "request_id_generator", sequenceName = "request_id_seq", allocationSize = 1)
+    private Long id;
+    private String name;
+    private String description;
+    private Double amount;
+}
